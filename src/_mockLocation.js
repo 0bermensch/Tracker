2f985c4b5b3a757277777b81c41519f9ac202dc7
Jason <jason.lo.kc@gmail.com>
2020-10-17 00:19:33 -0700

    some updates on creating and recording the tracks

--- src/_mockLocation.js
+++ src/_mockLocation.js
@@ -1,8 +1,8 @@
-import * as Location from 'expo-location'
+import * as Location from "expo-location";
 
-const tenMeterWithDegrees = 0.0001
+const tenMeterWithDegrees = 0.0001;
 
-const getLocation = increment => {
+const getLocation = (increment) => {
   return {
     timestamp: 10000000,
     coords: {
@@ -11,17 +11,17 @@ const getLocation = increment => {
       accuracy: 5,
       altitudeAccracy: 5,
       altitude: 5,
-      longitude: -122.0312186 + increment * tenMeterWithDegrees,
-      latitude: 37.33233141 + increment * tenMeterWithDegrees
-    }
-  }
-}
+      longitude: -123.144217 + increment * tenMeterWithDegrees,
+      latitude: 49.261061 + increment * tenMeterWithDegrees,
+    },
+  };
+};
 
-let counter = 0
+let counter = 0;
 setInterval(() => {
-  Location.EventEmitter.emit('Expo.locationChange', {
+  Location.EventEmitter.emit("Expo.locationChange", {
     watchId: Location._getCurrentWatchId(),
-    location: getLocation(counter)
+    location: getLocation(counter),
   });
-  counter++
-}, 1000)
+  counter++;
+}, 1000);
